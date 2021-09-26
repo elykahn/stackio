1feff633e69eabecea6d702523a4c8c5d888aaf1
Ely Kahn <ely.kahn@gmail.com>
2021-09-25 21:43:27 -0500

    Personalize config

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,24 +2,24 @@ const path = require('path');
 
 module.exports = {
     siteMetadata: {
-        title: `Novela by Narative`,
-        name: `Narative`,
-        siteUrl: `https://gatsby-theme-novela.netlify.com`,
+        title: `ely.io`,
+        name: `ely.io`,
+        siteUrl: `https://stackio-101d6.netlify.app`,
         description: `This is my description that will be used in the meta tags and important for search results`,
 
         // important to set the main text that appears in the hero
         hero: {
-            heading: `Perspectives on technology, design and business from the team at Narative.`,
+            heading: `Perspectives on technology, design and business from the mind of ely kahn.`,
             maxWidth: 652,
         },
         social: [
             {
                 name: `twitter`,
-                url: `https://twitter.com/narative`,
+                url: `https://twitter.com/creativ_ely`,
             },
             {
                 name: `github`,
-                url: `https://github.com/narative`,
+                url: `https://github.com/elykahn`,
             },
         ],
     },
